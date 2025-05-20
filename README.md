f8eb33057dc8439267d165dff68f4af347a88c8b
domogonotoso <argentinachan1998@gmail.com>
2025-05-20 16:03:03 +0900

    Revise slightly MNIST model for CIFAR-10 and add this with README.md

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # CIFAR-10 Image classification with Pytorch
 
 ## What is the objective?
-The main objective is write a code of validation. So I need to divide dataset and then write a code of train and valid in a one python file.
-And then write test python code.
+The first main objective is write a code of validation. So I need to divide dataset and then write a code of train and valid in a one python file.
+And the second main objective is comparing different ML models. So make models file directory to organize models.
+
+## CIFAR input shape
+input shape : (3 x 32 x 32)
+The number 3 means RGB color channel. 
+
+output shape : (10, 1) --> 10 classes
+
+## CNN
+convolution 2D
+Maxpool 2D
+These two filter has same attribute that they has a square filter. But convolution 2D has so many parameters but Maxpool just pick max value in its square filter. And generally maxpool2D squeeze output shape radically more than Conv2D.
---
+++ models/MNIST.py
@@ -0,0 +1,24 @@
+# MNIST.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MnistCNN(nn.Module):
+    def __init__(self):
+        super(MnistCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)  # Unlike MNIST, CIFAR-10 starts with 3 channels, RGB.
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)  
+        self.dropout = nn.Dropout(0.25) 
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)  
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))       # (batch, 32, 16, 16)
+        x = self.pool(F.relu(self.conv2(x)))       # (batch, 64, 8, 8)
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1) # Make code more generally. Automatically multiply x.shape except mini batch size.   
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
